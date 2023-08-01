38c40c8c18467f6c90f378c6d28e077284809374
munnep <patrick.munne@hashicorp.com>
2023-08-01 15:43:54 +0200

    corrected the code

--- main.tf
+++ main.tf
@@ -1,23 +1,23 @@
 resource "null_resource" "a_single_resource" {}
 
 check "check_url_succeed" {
-  data "http" "terraform_io" {
+  data "http" "check_url_succeed" {
     url = "https://terraform.io"
   }
 
   assert {
-    condition = data.http.terraform_io.status_code == 200
-    error_message = "${data.http.terraform_io.url} returned an unhealthy status code"
+    condition = data.http.check_url_succeed.status_code == 200
+    error_message = "${data.http.check_url_succeed.url} returned an unhealthy status code"
   }
 }
 
 check "check_url_fail" {
-  data "http" "terraform_io" {
+  data "http" "check_url_fail" {
     url = "https://terraform_patrick.io"
   }
 
   assert {
-    condition = data.http.terraform_io.status_code == 200
-    error_message = "${data.http.terraform_io.url} returned an unhealthy status code"
+    condition = data.http.check_url_fail.status_code == 200
+    error_message = "${data.http.check_url_fail.url} returned an unhealthy status code"
   }
 }
